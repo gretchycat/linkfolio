30972ac5c0c874060d3ef5eb387820e65ac8a259
Gretchen Maculo <gretchynm@gmail.com>
2025-05-06 13:19:58 -0500

    New Link form at the end of each category in the link settings

--- includes/23-link-loop.php
+++ includes/23-link-loop.php
@@ -3,24 +3,24 @@
 
 defined('ABSPATH') || exit;
 
-function lf_render_all_links() 
+function lf_render_all_links()
 {
     global $wpdb;
     $table = $wpdb->prefix . 'custom_links';
     $links = $wpdb->get_results("SELECT * FROM $table ORDER BY id ASC");
-    foreach ($links as $link) 
+    foreach ($links as $link)
     {
         $id = $link->id;
-        if (isset($_POST["edit_link_$id"])) 
+        if (isset($_POST["edit_link_$id"]))
         {
             lf_render_link_row_editor($link);
-        } elseif (isset($_POST["saved_link_$id"])) 
+        } elseif (isset($_POST["saved_link_$id"]))
         {
             lf_render_link_row_view($link);
-        } elseif (isset($_POST["cancel_link_triggered_$id"])) 
+        } elseif (isset($_POST["cancel_link_triggered_$id"]))
         {
             lf_render_link_row_view($link);
-        } else 
+        } else
         {
             lf_render_link_row_view($link);
         }
@@ -33,33 +33,33 @@ function lf_render_all_links()
     echo '<p><button class="button" name="add_new_link" value="1">+ Add Link</button></p>';
 }
 
-function lf_render_links_by_category($slug, $show_broken) 
+function lf_render_links_by_category($slug, $show_broken)
 {
     global $wpdb;
     $table = $wpdb->prefix . 'custom_links';
     $links = $wpdb->get_results($wpdb->prepare("SELECT * FROM $table WHERE category_slug = %s ORDER BY id ASC", $slug));
 
-    foreach ($links as $link) 
+    foreach ($links as $link)
     {
         $id = $link->id;
-        if (isset($_POST["edit_link_$id"])) 
+        if (isset($_POST["edit_link_$id"]))
         {
             lf_render_link_row_editor($link);
-        } 
-        elseif (isset($_POST["saved_link_$id"])) 
+        }
+        elseif (isset($_POST["saved_link_$id"]))
         {
             lf_render_link_row_view($link, true, $show_broken);
-        } 
-        elseif (isset($_POST["cancel_link_triggered_$id"])) 
+        }
+        elseif (isset($_POST["cancel_link_triggered_$id"]))
         {
             lf_render_link_row_view($link, true, $show_broken);
-        } 
-        else 
+        }
+        else
         {
             lf_render_link_row_view($link, true, $show_broken);
         }
     }
-    $newlink = [
+    $newlink = (object)[
         'id'          => 'new',
         'label'       => '',
         'url'         => '',
