d3b7790190915188e0b305e5f6d0c7b1235861cd
Gretchen Maculo <gretchynm@gmail.com>
2025-05-06 15:24:26 -0500

    meta box render tuning

--- includes/30-link-meta-box.php
+++ includes/30-link-meta-box.php
@@ -16,7 +16,7 @@ function lf_render_link_meta_box($post)
     $skipped_broken_links = 0;
     foreach ($all_links as $link)
     {
-        if (trim(strtolower($link->label)) === '404') {
+        if (trim(strtolower(floor($link->statuss_code/100)!=4)) === 4
             $skipped_broken_links++;
             continue;
         }
