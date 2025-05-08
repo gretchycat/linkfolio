42e2839f036a2e416ddc15a1af15f3643747ce74
Gretchen Maculo <gretchynm@gmail.com>
2025-05-08 17:31:22 -0500

    starting to work on broken links handling again

--- includes/42-rescan-broken-links.php
+++ includes/42-rescan-broken-links.php
@@ -12,7 +12,7 @@ function lf_rescan_broken_links()
     if (empty($broken_links)) return;
 
     foreach($broken_links as $bl)
-        lf_rescan_broken_link($bl->url)
+        lf_rescan_broken_link($bl->url);
 }
 
 function lf_rescan_broken_link($url)
