0d87a65f73e5970e56cc7b319ad0b34573909337
Gretchen Maculo <gretchynm@gmail.com>
2025-05-08 22:23:05 -0500

    bulk broken links handling

--- includes/42-rescan-broken-links.php
+++ includes/42-rescan-broken-links.php
@@ -10,11 +10,17 @@ function lf_rescan_broken_links()
     // Get all broken links attached to this post/page
     $broken_links = $wpdb->get_results( "SELECT * FROM $table WHERE status_code BETWEEN 400 AND 499");
     if (empty($broken_links)) return;
-
+    $links=[];
     foreach($broken_links as $bl)
     {
         $bl->label='';
+        $links[]=$bl->url
         lf_save_link(get_object_vars($bl), $bl->id);
     }
+    add_action('admin_notices', function () use ($titles, $links) {
+        echo "<div class='notice notice-success is-dismissible'><p>";
+        printf('Rescanned broken link(s) [%s]', implode(', ',$links));
+        echo "</p></div>";
+
 }
 
