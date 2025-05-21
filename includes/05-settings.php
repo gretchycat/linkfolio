cf04e9b7ae0b42a141500bde884d2a7234bc5b37
Gretchen Maculo <gretchynm@gmail.com>
2025-05-20 20:35:02 -0500

    Docs printing

--- includes/05-settings.php
+++ includes/05-settings.php
@@ -2,7 +2,6 @@
 // 05-settings.php
 defined('ABSPATH') || exit;
 require_once plugin_dir_path(__FILE__) . 'lib/Parsedown/Parsedown.php';
-// Add main Linkfolio admin menu with subpages
 add_action('admin_menu', function () {
     add_menu_page(
         'Linkfolio',
