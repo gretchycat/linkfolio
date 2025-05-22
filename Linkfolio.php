50058e49f5dd4aea4f2c7646413ee736d4839556
Gretchen Maculo <gretchynm@gmail.com>
2025-05-21 19:18:19 -0500

    shortcode support

--- Linkfolio.php
+++ Linkfolio.php
@@ -87,13 +87,3 @@ add_action('admin_enqueue_scripts', function() {
 
 $plugins['linkfolio_shortcode'] = plugins_url('assets/linkfolio-tinymce.js', __FILE__);
 
-add_action('admin_init', function() {
-    add_filter('mce_external_plugins', function($plugins) {
-        $plugins['linkfolio_shortcode'] = plugin_dir_url(__FILE__) . 'assets/linkfolio-tinymce.js';
-        return $plugins;
-    });
-    add_filter('mce_buttons', function($buttons) {
-        array_push($buttons, 'linkfolio_shortcode');
-        return $buttons;
-    });
-});
