9849a103279a744a74bca828ade5325617c8f3e3
Gretchen Maculo <gretchynm@gmail.com>
2025-05-21 19:12:29 -0500

    shortcode support

--- Linkfolio.php
+++ Linkfolio.php
@@ -36,3 +36,64 @@ add_filter('plugin_action_links_' . plugin_basename(__FILE__), function ($links)
     return $links;
 });
 
+function linkfolio_register_block()
+{
+    wp_register_script(
+        'linkfolio-block-script',
+        plugins_url('assets/block/index.js', __FILE__),
+        [ 'wp-blocks', 'wp-element', 'wp-editor', 'wp-components', 'wp-api-fetch' ],
+        filemtime(plugin_dir_path(__FILE__) . 'assets/block/index.js')
+    );
+
+    register_block_type(
+        'linkfolio/shortcode',
+        array(
+            'editor_script' => 'linkfolio-block-script',
+        )
+    );
+}
+add_action('init', 'linkfolio_register_block');
+
+add_action('rest_api_init', function() {
+    register_rest_route('linkfolio/v1', '/categories', array(
+        'methods' => 'GET',
+        'callback' => function() {
+            global $wpdb;
+            $table = $wpdb->prefix . 'linkfolio_link_categories';
+            $rows = $wpdb->get_results("SELECT name, slug FROM $table ORDER BY name ASC");
+            return $rows;
+        },
+        'permission_callback' => function() {
+            return current_user_can('edit_posts');
+        }
+    ));
+});
+
+add_action('admin_init', function () {
+    add_filter('mce_external_plugins', function ($plugins) {
+        $plugins['linkfolio_shortcode'] = plugins_url('assets/linkfolio-tinymce.js', __FILE__);
+        return $plugins;
+    });
+    add_filter('mce_buttons', function ($buttons) {
+        array_push($buttons, 'linkfolio_shortcode');
+        return $buttons;
+    });
+});
+
+add_action('admin_enqueue_scripts', function() {
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('wp-api');
+});
+
+$plugins['linkfolio_shortcode'] = plugins_url('assets/linkfolio-tinymce.js', __FILE__);
+
+add_action('admin_init', function() {
+    add_filter('mce_external_plugins', function($plugins) {
+        $plugins['linkfolio_shortcode'] = plugin_dir_url(__FILE__) . 'assets/linkfolio-tinymce.js';
+        return $plugins;
+    });
+    add_filter('mce_buttons', function($buttons) {
+        array_push($buttons, 'linkfolio_shortcode');
+        return $buttons;
+    });
+});
